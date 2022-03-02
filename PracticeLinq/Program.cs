657f45d21d561a42dddc1996c1ecf0f01050a227
Valdir Lima <valdirlima508@gmail.com>
2022-03-02 16:08:06 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Order the list of games by length of the game name
+            var videoGames = new List<string> { "Dark Souls", "Sifu", "The Witcher 3", "Fifa 21" };
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+        }
+        
+    }
+}
